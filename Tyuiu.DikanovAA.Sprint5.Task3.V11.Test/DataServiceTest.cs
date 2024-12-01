3292b468eff3c060448fd09be0ea2d13eb3f2a1e
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-12-01 23:48:08 +0500

    Task3

---
+++ Tyuiu.DikanovAA.Sprint5.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.DikanovAA.Sprint5.Task3.V17.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.DikanovAA.Sprint5.Task3.V11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.DikanovAA.Sprint5.Task3.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
