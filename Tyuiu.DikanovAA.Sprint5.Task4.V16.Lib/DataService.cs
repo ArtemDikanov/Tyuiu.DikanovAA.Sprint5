942ef5fdf4010a261d70970a7bf7cf91728704c6
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-12-02 11:56:21 +0500

    Task4

---
+++ Tyuiu.DikanovAA.Sprint5.Task4.V16.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.DikanovAA.Sprint5.Task4.V16.Lib
+{
+    public class DataService : ISprint5Task4V16
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double x = Convert.ToDouble(strX);
+            double res = Math.Round((Math.Cos(x) + Math.Pow(x, 2) - ((2 * x) / 1.2)), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.DikanovAA.Sprint5.Task4.V16/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.DikanovAA.Sprint5.Task4.V16.Lib;
+namespace Tyuiu.DikanovAA.Sprint5.Task4.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V16.txt";
+            Console.WriteLine("Результат: ");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
