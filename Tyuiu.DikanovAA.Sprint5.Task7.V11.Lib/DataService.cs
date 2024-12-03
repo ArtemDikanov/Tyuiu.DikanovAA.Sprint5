3399dc27b219cf0bc6fc1041123c0b191cfcb743
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-12-03 21:41:15 +0500

    Task7

---
+++ Tyuiu.DikanovAA.Sprint5.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.DikanovAA.Sprint5.Task7.V11.Lib
+{
+    public class DataService : ISprint5Task7V11
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V11.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if ((line[i] != 'а') && (line[i] != 'б') && (line[i] != 'в') && (line[i] != 'г') && (line[i] != 'д') && (line[i] != 'е') && (line[i] != 'ё') && (line[i] != 'ж') && (line[i] != 'з') && (line[i] != 'и') && (line[i] != 'й') && (line[i] != 'к') && (line[i] != 'л') && (line[i] != 'м') && (line[i] != 'н') && (line[i] != 'о') && (line[i] != 'п') && (line[i] != 'р') && (line[i] != 'с') && (line[i] != 'т') && (line[i] != 'у') && (line[i] != 'ф') && (line[i] != 'х') && (line[i] != 'ц') && (line[i] != 'ч') && (line[i] != 'ш') && (line[i] != 'щ') && (line[i] != 'ъ') && (line[i] != 'ы') && (line[i] != 'ь') && (line[i] != 'э') && (line[i] != 'ю') && (line[i] != 'я'))
+                        {
+                            strLine = strLine + line[i];
+                        }
+
+                    }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.DikanovAA.Sprint5.Task7.V11/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.DikanovAA.Sprint5.Task7.V11.Lib;
+namespace Tyuiu.DikanovAA.Sprint5.Task7.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V11.txt";
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V11.txt";
+            Console.WriteLine("Результат: ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
